84e11697bf01b0ed1e8ac80d7703ca2fa7919a8b
Lewis Lovett <llovett7575@gmail.com>
2022-11-17 14:42:25 +0000

    Created words class

--- src/com/nology/hangman/Words.java
+++ src/com/nology/hangman/Words.java
@@ -1,4 +1,26 @@
 package com.nology.hangman;
 
+import java.util.ArrayList;
+
 public class Words {
+
+    ArrayList<String> words = new ArrayList<>();
+
+    public Words (){
+        this.words.add("approach");
+        this.words.add("sequence");
+        this.words.add("tropical");
+        this.words.add("epicalyx");
+        this.words.add("fraction");
+        this.words.add("portrait");
+        this.words.add("finished");
+    }
+
+    public ArrayList<String> getWords() {
+        return words;
+    }
+
+    public void setWords(ArrayList<String> words) {
+        this.words = words;
+    }
 }
